8b3792e61e66a9b0efb2d98e322ea33a56b481c3
Pablo Vieira <pablo.vieira@edu.pucrs.br>
2021-10-10 20:58:35 -0300

    It was added comments

--- test/TestBar.c
+++ test/TestBar.c
@@ -16,18 +16,20 @@ int values[] = { 40, 10, 100, 90, 20, 25 };
 int expected[] = { 10, 20, 25, 40, 90, 100 };
 int n = 1;
 
-TEST(Bar, TestBar1)
+TEST(Bar, TestBar1) // Testa se o vetor de entrada sai como esperado
 {
   sort(values, n);
   TEST_ASSERT_EQUAL_INT_ARRAY (expected, values, n);
 }
 
+// Testa se o tamanho do vetor é igual ao número de elementos a serem ordenados
 TEST(Bar, TestBar2)
 {
   int size = sizeof(values) / sizeof(int);
   TEST_ASSERT_EQUAL_INT (size, n);
 }
 
+// Testa se o tamanho do vetor se mantém igual ao número de elementos a serem ordenados
 TEST(Bar, TestBar3)
 {
   sort(values, n);
@@ -35,19 +37,21 @@ TEST(Bar, TestBar3)
   TEST_ASSERT_EQUAL_INT (size, n);
 }
 
+// Testa se o número de elementos a serem ordenados é maior que zero
 TEST(Bar, TestBar4)
 {
   sort(values, n);
-  int size = sizeof(values) / sizeof(int);
   TEST_ASSERT_GREATER_THAN (0, n);
 }
 
+// Testa se o tamanho do array a ser ordenado é maior que zero
 TEST(Bar, TestBar5)
 {
   int size = sizeof(values) / sizeof(int);
   TEST_ASSERT_GREATER_THAN (0, size);
 }
 
+// Testa se o tamanho do array a ser ordenado se mantém que zero
 TEST(Bar, TestBar6)
 {
   sort(values, n);
