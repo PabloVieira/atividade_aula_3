bd3f5d3d0ace23324773746c795ec6b5420efcf3
Pablo Vieira <pablo.vieira@edu.pucrs.br>
2021-10-10 20:50:01 -0300

    Some more tests

--- test/TestBar.c
+++ test/TestBar.c
@@ -14,10 +14,43 @@ TEST_TEAR_DOWN(Bar)
 
 int values[] = { 40, 10, 100, 90, 20, 25 };
 int expected[] = { 10, 20, 25, 40, 90, 100 };
-int n = 6;
+int n = 1;
 
 TEST(Bar, TestBar1)
 {
   sort(values, n);
   TEST_ASSERT_EQUAL_INT_ARRAY (expected, values, n);
+}
+
+TEST(Bar, TestBar2)
+{
+  int size = sizeof(values) / sizeof(int);
+  TEST_ASSERT_EQUAL_INT (size, n);
+}
+
+TEST(Bar, TestBar3)
+{
+  sort(values, n);
+  int size = sizeof(values) / sizeof(int);
+  TEST_ASSERT_EQUAL_INT (size, n);
+}
+
+TEST(Bar, TestBar4)
+{
+  sort(values, n);
+  int size = sizeof(values) / sizeof(int);
+  TEST_ASSERT_GREATER_THAN (0, n);
+}
+
+TEST(Bar, TestBar5)
+{
+  int size = sizeof(values) / sizeof(int);
+  TEST_ASSERT_GREATER_THAN (0, size);
+}
+
+TEST(Bar, TestBar6)
+{
+  sort(values, n);
+  int size = sizeof(values) / sizeof(int);
+  TEST_ASSERT_GREATER_THAN (0, size);
 }
--- test/test_runners/TestBar_Runner.c
+++ test/test_runners/TestBar_Runner.c
@@ -4,4 +4,9 @@
 TEST_GROUP_RUNNER(Bar)
 {
   RUN_TEST_CASE(Bar, TestBar1);
+  RUN_TEST_CASE(Bar, TestBar2);
+  RUN_TEST_CASE(Bar, TestBar3);
+  RUN_TEST_CASE(Bar, TestBar4);
+  RUN_TEST_CASE(Bar, TestBar5);
+  RUN_TEST_CASE(Bar, TestBar6);
 }
